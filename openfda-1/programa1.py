e4012b4827440fd69f12fbca8e31d19efb7cedaf
mmateos <myriam.mateos@outlook.es>
2018-03-23 18:04:14 +0100

    Funciona

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -1,75 +1,14 @@
-import http.server
-import socketserver
+import json
+import urllib
 
-# -- Puerto donde lanzar el servidor
-PORT = 8001
+data = urllib.urlopen("https://api.fda.gov/drug/label.json").read()
+output = json.loads(data)
 
+identificador = output['results'][0]['id']
+fabricante = output['results'][0]['questions'][0]
+proposito = output['results'][0]['purpose'][0]
 
-# Clase con nuestro manejador. Es una clase derivada de BaseHTTPRequestHandler
-# Esto significa que "hereda" todos los metodos de esta clase. Y los que
-# nosotros consideremos los podemos reemplazar por los nuestros
-class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+print ("El identificador es: {}".format(identificador))
+print ("El proposito es: {}".format(proposito))
+print ("El fabricante es: {}".format(fabricante))
 
-    # GET. Este metodo se invoca automaticamente cada vez que hay una
-    # peticion GET por HTTP. El recurso que nos solicitan se encuentra
-    # en self.path
-    def do_GET(self):
-
-        # La primera linea del mensaje de respuesta es el
-        # status. Indicamos que OK
-        self.send_response(200)
-
-        # En las siguientes lineas de la respuesta colocamos las
-        # cabeceras necesarias para que el cliente entienda el
-        # contenido que le enviamos (que sera HTML)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-        # Este es el mensaje que enviamos al cliente: un texto y
-        # el recurso solicitado
-        filename = ""
-        if self.path == "/":
-            filename = "green.html"
-        else:
-            if (self.path == "/blue"):
-                filename = "blue.html"
-            else:
-                filename = "pink.html"
-
-        print("Fichero a servir: {}".format(filename))
-        with open(filename, "r") as f:
-            contenido = f.read()
-
-        message = contenido
-
-        # Enviar el mensaaje completo
-        self.wfile.write(bytes(message, "utf8"))
-        print("File served!")
-        return
-
-
-# ----------------------------------
-# El servidor comienza a aqui
-# ----------------------------------
-# Establecemos como manejador nuestra propia clase
-Handler = testHTTPRequestHandler
-
-# -- Configurar el socket del servidor, para esperar conexiones de clientes
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-
-# Entrar en el bucle principal
-# Las peticiones se atienden desde nuestro manejador
-# Cada vez que se ocurra un "GET" se invoca al metodo do_GET de
-# nuestro manejador
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    print("")
-    print("Interrumpido por el usuario")
-
-print("")
-print("Servidor parado")
-httpd.close()
-
-# https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
