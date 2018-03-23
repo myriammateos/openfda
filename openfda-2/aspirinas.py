941e1a2efcd59aa96539ddbc3f5f0b4665301f0c
mmateos <myriam.mateos@outlook.es>
2018-03-23 19:04:44 +0100

    Corregido el fallo de KeyError para cuando el fabricante no viene especificado

--- openfda-2/aspirinas.py
+++ openfda-2/aspirinas.py
@@ -3,10 +3,15 @@ import urllib
 
 
 num_drug = 100
+active = "acetylsalicylic"
 
-data = urllib.urlopen('https://api.fda.gov/drug/label.json?search=active_ingredient:"acetylsalicylic%22&limit=100').read()
+data = urllib.urlopen('https://api.fda.gov/drug/label.json?search=active_ingredient:"{}"&limit={}'.format(active,num_drug)).read()
 output = json.loads(data)
 
-for i in range(num_drug):
-    fabricante = output['results'][i]['openfda']['manufacturer_name'][0]
-    print(fabricante)
+print("Fabricantes de aspirinas:")
+for i in range(len(output['results'])):
+    try:
+        fabricante = output['results'][i]['openfda']['manufacturer_name'][0]
+        print(fabricante)
+    except KeyError:
+        print("No especificado")
