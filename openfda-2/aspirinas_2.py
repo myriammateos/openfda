9424a7036a3b8aeeeb1127e7b5dc6d5918ac6a92
mmateos <myriam.mateos@outlook.es>
2018-04-05 15:08:50 +0200

    Otra forma de hacer el ejercicio de las aspirinas

---
+++ openfda-2/aspirinas_2.py
@@ -0,0 +1,26 @@
+import http.client
+import json
+
+web = "api.fda.gov"  # -- Nombre del servidor REST
+resource = "/drug/label.json"
+headers = {'User-Agent': 'http-client'}
+num_drug = 100
+active = "acetylsalicylic"
+extra = '?search=active_ingredient:"{}"&limit={}'.format(active,num_drug)
+
+conexion = http.client.HTTPSConnection(web)
+conexion.request("GET", resource+extra, None, headers)
+response = conexion.getresponse()
+
+data = response.read().decode("utf-8")
+conexion.close()
+
+output = json.loads(data)
+
+print("Fabricantes de aspirinas:")
+for i in range(len(output['results'])):
+    try:
+        fabricante = output['results'][i]['openfda']['manufacturer_name'][0]
+        print("  -" + fabricante)
+    except KeyError:
+        print("  -No especificado")
