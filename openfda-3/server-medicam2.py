90d09169b1737d59cf9c6af43a5320b5d592e0fe
mmateos <myriam.mateos@outlook.es>
2018-04-06 17:20:58 +0200

    Depurado

---
+++ openfda-3/server-medicam2.py
@@ -0,0 +1,103 @@
+import http.server
+import socketserver
+import json
+import http.client
+import urllib.request
+
+#Info
+
+web = "api.fda.gov"
+resource = "/drug/label.json"
+headers = {'User-Agent': 'http-client'}
+
+
+PORT = 8000
+IP = "" #Por defecto coge la IP local 127.0.0.1
+num_drug = 10
+url = "https://api.fda.gov/drug/label.json?limit={}".format(num_drug)
+extra = '?limit={}'.format(num_drug)
+
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # Status
+        self.send_response(200)
+
+        # Que contenido estamos pasando
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Este es el mensaje que enviamos al cliente: un texto y el recurso solicitado
+        message = """<!doctype html>
+                <html>
+                  <body style='background-color:#545454'>
+                    <font color="white">
+                    <h1>LISTA DE MEDICAMENTOS</h2>"""
+
+        conexion = http.client.HTTPSConnection(web)
+        conexion.request("GET", resource + extra, None, headers)
+        response = conexion.getresponse()
+
+        data = response.read().decode("utf-8")
+        conexion.close()
+
+        output = json.loads(data)
+
+        message += """</p><table style="width:auto" class="width:egt" border="2">
+                      <tr>
+                        <th>Medicamento</th>
+                        <th>Identificador</th>
+                        <th>Principio activo</th>
+                      </tr>"""
+
+        for i in range(num_drug):
+            try:
+                name = output['results'][i]['openfda']['substance_name'][0]
+            except KeyError:
+                name = "No especificado"
+            identificador = output['results'][i]['id']
+            try:
+                active = output['results'][i]['active_ingredient'][0]
+            except KeyError:
+                active = "No especificado"
+
+            message += "<tr>"
+            message += "<td>{}</th>".format(name)
+            message += "<td>{}</th>".format(identificador)
+            message += "<td>{}</th>".format(active)
+            message += "</tr>"
+
+        message += """</tr></tbody></table>
+                    </font>
+                    </body></html>"""
+
+        # Enviar el mensaaje completo
+        self.wfile.write(bytes(message, "utf8"))
+        print("File served!")
+        return
+
+
+# ----------------------------------
+# El servidor comienza a aqui
+# ----------------------------------
+
+# Establecemos como manejador nuestra propia clase
+Handler = testHTTPRequestHandler
+
+# -- Configurar el socket del servidor, para esperar conexiones de clientes
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("Serving at port", PORT)
+
+# Entrar en el bucle principal
+# Las peticiones se atienden desde nuestro manejador
+# Cada vez que se ocurra un "GET" se invoca al metodo do_GET de nuestro manejador
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("")
+    print("Interrumpido por el usuario")
+
+print("")
+print("Servidor parado")
+httpd.server_close()
