9d15bc5eb5fa9511be0580f5abf2c67793b8ed02
mmateos <myriam.mateos@outlook.es>
2018-04-06 18:32:17 +0200

    Revisado

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -4,8 +4,16 @@ import urllib.request
 num_drug = 10
 url = "https://api.fda.gov/drug/label.json?limit={}".format(num_drug)
 
-data = urllib.request.urlopen(url).read().decode("utf-8")
-output = json.loads(data)
+try:
+    data = urllib.request.urlopen(url).read().decode("utf-8")
+    output = json.loads(data)
+except urllib.error.URLError as error:
+    if error.code == 404:
+        print("Error 404, page not found")
+    else:
+        print("Another error occurred")
+    print("La URL {} es erronea".format(url))
+    exit(1)
 
 for i in range(num_drug):
     identificador = output['results'][i]['id']
